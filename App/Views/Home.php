bc14d71bc762e5c512e0ab1b1a85bf85cb1a1cbb
lopatar <github@lopatar.me>
2022-11-19 11:53:16 +0100

    Refactor config system

--- App/Views/Home.php
+++ App/Views/Home.php
@@ -3,14 +3,14 @@ declare(strict_types=1);
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
-	<head>
-		<title>Home view</title>
-		<meta charset="UTF-8">
-		<meta content="width=device-width, initial-scale=1" name="viewport">
-	</head>
+<head>
+    <title>Home view</title>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+</head>
 
-	<body>
-		<p>Welcome: <?= $this->getProperty('user')->username ?></p>
-	</body>
+<body>
+<p>Welcome: <?= $this->getProperty('user')->username ?></p>
+</body>
 </html>
 
--- README.md
+++ README.md
@@ -9,13 +9,16 @@ primitive [PHP SDK](https://github.com/lopatar/PHP-SDK) (which is now archived).
 - Composer
 - Web server (routing all requests to public/index.php)
 
-
 # Installation
+
 To install the SDK, please run the following composer command
+
 ```shell
 composer create-project lopatar/php-sdkv2 <PROJECT-NAME>
 ```
+
 A folder called PROJECT-NAME will be created, please point your web server to /path/to/<PROJECT-NAME>/public/index.php
+
 # Routing requests to index.php
 
 - NGINX
