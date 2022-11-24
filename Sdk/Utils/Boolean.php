b1ec8292a6c29afe263b250c1e35be0037f7289c
Jiří Lopatář <github@lopatar.me>
2022-11-24 10:01:51 +0100

    Create boolean util class

---
+++ Sdk/Utils/Boolean.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sdk\Utils;
+
+abstract class Boolean
+{
+    /**
+     * This function accepts a string value that is then being converted to bool, acceptable values are 'true' and 'false'
+     * @param string $value
+     * @return bool|null Null on failure
+     */
+    public static function fromString(string $value): ?bool
+    {
+        if ($value === 'true') {
+            return true;
+        }
+
+        if ($value === 'false') {
+            return false;
+        }
+
+        return null;
+    }
+}
